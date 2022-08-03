77b3e983ac364be5503c8d4c4843b2d9a1edea29
Richard <ri_chardfilipe@hotmail.com>
2022-08-02 22:13:09 -0300

    MinimalPronta

---
+++ MinimalDoMinimalAPI/MyEndpoint.cs
@@ -0,0 +1,19 @@
+﻿using FastEndpoints;
+
+namespace MinimalDoMinimalAPI
+{
+    public class MyEndpoint : EndpointWithoutRequest
+    {
+        public override void Configure()
+        {
+            Verbs(Http.GET);
+            Routes("/endzinho");
+            AllowAnonymous();
+        }
+
+        public override async Task HandleAsync(CancellationToken ct)
+        {
+            await SendAsync(new { message = "OláFastEndpoint" });
+        }
+    }
+}
